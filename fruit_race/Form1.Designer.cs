9f03959541c92740174d6cc32f12f96bf5bdc89e
Yavuz Nazikgül <yavzrcp@outlook.com>
2022-02-12 13:28:55 +0300

    Add files via upload

---
+++ fruit_race/Form1.Designer.cs
@@ -0,0 +1,376 @@
+﻿namespace fruit_race
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.panel12 = new System.Windows.Forms.Panel();
+            this.panel13 = new System.Windows.Forms.Panel();
+            this.panel14 = new System.Windows.Forms.Panel();
+            this.panel15 = new System.Windows.Forms.Panel();
+            this.panel16 = new System.Windows.Forms.Panel();
+            this.panel17 = new System.Windows.Forms.Panel();
+            this.panel18 = new System.Windows.Forms.Panel();
+            this.panel19 = new System.Windows.Forms.Panel();
+            this.panel20 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel1.Location = new System.Drawing.Point(48, 336);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(30, 30);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel2.Location = new System.Drawing.Point(48, 300);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(30, 30);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel3.Location = new System.Drawing.Point(48, 264);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(30, 30);
+            this.panel3.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel4.Location = new System.Drawing.Point(48, 228);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(30, 30);
+            this.panel4.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel5.Location = new System.Drawing.Point(48, 192);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(30, 30);
+            this.panel5.TabIndex = 0;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel6.Location = new System.Drawing.Point(48, 156);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(30, 30);
+            this.panel6.TabIndex = 0;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel7.Location = new System.Drawing.Point(48, 120);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(30, 30);
+            this.panel7.TabIndex = 0;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel8.Location = new System.Drawing.Point(48, 84);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(30, 30);
+            this.panel8.TabIndex = 0;
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel9.Location = new System.Drawing.Point(48, 48);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(30, 30);
+            this.panel9.TabIndex = 0;
+            // 
+            // panel10
+            // 
+            this.panel10.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.panel10.Location = new System.Drawing.Point(48, 12);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(30, 30);
+            this.panel10.TabIndex = 0;
+            // 
+            // panel11
+            // 
+            this.panel11.BackColor = System.Drawing.Color.Yellow;
+            this.panel11.Location = new System.Drawing.Point(12, 336);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(30, 30);
+            this.panel11.TabIndex = 0;
+            // 
+            // panel12
+            // 
+            this.panel12.BackColor = System.Drawing.Color.Red;
+            this.panel12.Location = new System.Drawing.Point(12, 228);
+            this.panel12.Name = "panel12";
+            this.panel12.Size = new System.Drawing.Size(30, 30);
+            this.panel12.TabIndex = 0;
+            // 
+            // panel13
+            // 
+            this.panel13.BackColor = System.Drawing.Color.Purple;
+            this.panel13.Location = new System.Drawing.Point(12, 120);
+            this.panel13.Name = "panel13";
+            this.panel13.Size = new System.Drawing.Size(30, 30);
+            this.panel13.TabIndex = 0;
+            // 
+            // panel14
+            // 
+            this.panel14.BackColor = System.Drawing.Color.Yellow;
+            this.panel14.Location = new System.Drawing.Point(12, 300);
+            this.panel14.Name = "panel14";
+            this.panel14.Size = new System.Drawing.Size(30, 30);
+            this.panel14.TabIndex = 0;
+            // 
+            // panel15
+            // 
+            this.panel15.BackColor = System.Drawing.Color.Red;
+            this.panel15.Location = new System.Drawing.Point(12, 192);
+            this.panel15.Name = "panel15";
+            this.panel15.Size = new System.Drawing.Size(30, 30);
+            this.panel15.TabIndex = 0;
+            // 
+            // panel16
+            // 
+            this.panel16.BackColor = System.Drawing.Color.Purple;
+            this.panel16.Location = new System.Drawing.Point(12, 84);
+            this.panel16.Name = "panel16";
+            this.panel16.Size = new System.Drawing.Size(30, 30);
+            this.panel16.TabIndex = 0;
+            // 
+            // panel17
+            // 
+            this.panel17.BackColor = System.Drawing.Color.Yellow;
+            this.panel17.Location = new System.Drawing.Point(12, 264);
+            this.panel17.Name = "panel17";
+            this.panel17.Size = new System.Drawing.Size(30, 30);
+            this.panel17.TabIndex = 0;
+            // 
+            // panel18
+            // 
+            this.panel18.BackColor = System.Drawing.Color.Red;
+            this.panel18.Location = new System.Drawing.Point(12, 156);
+            this.panel18.Name = "panel18";
+            this.panel18.Size = new System.Drawing.Size(30, 30);
+            this.panel18.TabIndex = 0;
+            // 
+            // panel19
+            // 
+            this.panel19.BackColor = System.Drawing.Color.Purple;
+            this.panel19.Location = new System.Drawing.Point(12, 48);
+            this.panel19.Name = "panel19";
+            this.panel19.Size = new System.Drawing.Size(30, 30);
+            this.panel19.TabIndex = 0;
+            // 
+            // panel20
+            // 
+            this.panel20.BackColor = System.Drawing.Color.Orange;
+            this.panel20.Location = new System.Drawing.Point(12, 12);
+            this.panel20.Name = "panel20";
+            this.panel20.Size = new System.Drawing.Size(30, 30);
+            this.panel20.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(84, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Play !!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Score:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(129, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "-";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(114, 48);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(75, 20);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(85, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Bet:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(85, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Money:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(129, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "1000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(201, 385);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel20);
+            this.Controls.Add(this.panel10);
+            this.Controls.Add(this.panel19);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel18);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel17);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel16);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel15);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel14);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel13);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel12);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel11);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.Panel panel12;
+        private System.Windows.Forms.Panel panel13;
+        private System.Windows.Forms.Panel panel14;
+        private System.Windows.Forms.Panel panel15;
+        private System.Windows.Forms.Panel panel16;
+        private System.Windows.Forms.Panel panel17;
+        private System.Windows.Forms.Panel panel18;
+        private System.Windows.Forms.Panel panel19;
+        private System.Windows.Forms.Panel panel20;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ fruit_race/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fruit_race
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int money;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            money = 1000;
+            label5.Text = money.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Color[] remainingColors = { //All colors have a certain number of uses
+            Color.Yellow,//0
+            Color.Yellow,//1st index
+            Color.Yellow,
+            Color.Red,
+            Color.Red,
+            Color.Red,
+            Color.Purple,
+            Color.Purple,
+            Color.Purple,//8
+            Color.Orange,//9
+        };
+            Color[] referanceColors = {
+            Color.Yellow,//0
+            Color.Yellow,//1st index
+            Color.Yellow,
+            Color.Red,
+            Color.Red,
+            Color.Red,
+            Color.Purple,
+            Color.Purple,
+            Color.Purple,//8
+            Color.Orange,//9
+        };
+
+            int bet = Decimal.ToInt32(numericUpDown1.Value);
+
+            if (bet <= money)
+            {
+                money -= bet;
+                label5.Text = money.ToString();
+
+                Random number = new Random(); //Creates a random function
+                int count = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    int numberRand = number.Next(0, remainingColors.Length); //Makes a choice from the remaining colors
+                    Panel pnl = this.Controls.Find("panel" + ((i + 1).ToString()), true).FirstOrDefault() as Panel; //Get each panel in turn
+                    if (remainingColors[numberRand] == referanceColors[i])
+                    {
+                        count++;
+                    }
+                    pnl.BackColor = remainingColors[numberRand]; //Apply the color
+                    remainingColors = remainingColors.Where((source, index) => index != numberRand).ToArray(); //Delete the applied color from array
+                }
+                label2.Text = count.ToString();
+
+                if (count > 3 || panel10.BackColor == Color.Orange)
+                {
+                    money += bet * 2;
+                    label5.Text = money.ToString();
+                }
+                else if (count == 3)
+                {
+                    money += bet;
+                    label5.Text = money.ToString();
+                }
+            }
+            else
+            {
+                MessageBox.Show("The budget cannot be less than the bet!");
+            }
+        }
+    }
+}
